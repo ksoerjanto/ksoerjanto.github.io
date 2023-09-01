2e80c3a880e51d8b467ce9551db89a2d05718d52
Karina Soerjanto <ksoerjanto@maxs-air.lan>
2023-08-31 20:30:04 -0700

    add nav bar to all pages. add initial home page content.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let menuOverlay = document.getElementById('menu-overlay');
+let image1 = document.getElementById('image-1');
+let downArrow = document.getElementById('arrow-container');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 20) {
+        downArrow.style.opacity = '0';
+    }
+});
 
 function toggleMenu(event) {
-    console.log('togglemenu');
     if (!menuOverlay.style.height || menuOverlay.style.height === '0vh') {
         menuOverlay.style.height = '100vh';
     } else {
         menuOverlay.style.height = '0vh';
     }
-    
 };
