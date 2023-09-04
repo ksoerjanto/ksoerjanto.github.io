fd598f838e58945aaabf2c4b3f4a4b9a6d551244
Karina Soerjanto <ksoerjanto@maxs-air.lan>
2023-09-03 20:10:11 -0700

    fix scroll animations for mobile

--- career.js
+++ career.js
@@ -1,18 +1,15 @@
 const cards = document.querySelectorAll('.card');
 
-console.log(cards.length);
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            console.log('isIntersecting');
             entry.target.classList.add('scroll-animations');
         }
         else {
-            console.log('is not intersecting');
-            entry.target.classList.remove('scroll-animation');
+            entry.target.classList.remove('scroll-animations');
         }
     });
-}, { threshold: 0.3 });
+}, { threshold: 0.4 });
 
 for (let i = 0; i < cards.length; i++) {
     const elements = cards[i];
